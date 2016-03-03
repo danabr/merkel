933e03ce3a280dbf380cd9bd86131ec6f14cad46
Daniel Abrahamsson <hamsson@gmail.com>
2016-03-03 11:59:22 +0100

    Make all functions generate ok

--- src/merkel_im.erl
+++ src/merkel_im.erl
@@ -22,37 +22,36 @@ from_lambda(["setglobal", _ModName, In]) ->
                             [cerl:abstract(test), cerl:ann_c_var([], x)]))
             }
           ],
-  Exports = exports(Funs),
-  cerl:c_module(cerl:abstract(test), Exports, CFuns).
-
-exports(Funs) ->
-  FromMod = lists:map(fun({N, A, _}) ->
-              cerl:c_fname(list_to_atom(N), A)
-            end, Funs),
-  [ cerl:c_fname(module_info, 0)
-  , cerl:c_fname(module_info, 1)
-  | FromMod
-  ].
+  Exports = lists:map(fun({N, _}) -> N end, Funs ++ CFuns),
+  cerl:c_module(cerl:abstract(test), Exports, Funs ++ CFuns).
 
 %% It is probably smarter to get the exports from makeblock.
-functions(["makeblock"|_])     -> []; 
-functions(["let", Funs, In])   -> 
+functions(["makeblock"|_])     -> [];
+functions(["let", Funs, In])   ->
   lets(Funs) ++ functions(In);
-functions(["letrec", Fun, In]) -> 
+functions(["letrec", Fun, In]) ->
   [function(Fun)|functions(In)].
 
 lets([])                    -> [];
 lets([Fun, "=", Impl|Lets]) ->
   [function([Fun, Impl])|lets(Lets)].
 
-function([Name, Fun]) -> {scrub_name(Name), arity(Fun), Fun}.
+function([Name, Fun]) ->
+  {cerl:c_fname(scrub_name(Name), arity(Fun)), func(Fun)}.
 
 arity(["function"|ArgsAndBody]) ->
   length(lists:droplast(ArgsAndBody)).
+
+func(["function"|ArgsAndBody]) ->
+  cerl:c_fun(args(ArgsAndBody), cerl:abstract(ok)).
+
+args([_Body])     -> [];
+args([Name|Rest]) ->
+  [cerl:ann_c_var([], scrub_name(Name))|args(Rest)].
 
 %% The ocaml ids are formatted like "fib/1001", to handle nested let
 %% bindings with the same identifier. Erlang does not support this.
 %% Skip for now.
 scrub_name(Str) ->
-  lists:takewhile(fun(C) -> C =/= $/ end, Str).
+  list_to_atom(lists:takewhile(fun(C) -> C =/= $/ end, Str)).
 
